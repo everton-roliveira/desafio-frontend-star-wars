b7f48eaedb87e786fb0c5116fdea759033519a73
Everton Roberto Oliveira <eroberto@everis.com>
2019-05-28 13:16:40 -0300

    Model de planet criada e implementada na view statica

---
+++ src/app/@core/models/planet.ts
@@ -0,0 +1,17 @@
+export class PlanetModel {
+    _id: string;
+    population: string;
+    name: string;
+    climate: string;
+    terrain: string;
+    films: number;
+
+    constructor() {
+        this._id = null;
+        this.population = null;
+        this.name = null;
+        this.climate = null;
+        this.terrain = null;
+        this.films = null;
+    }
+}
--- src/app/modules/game/game.component.ts
+++ src/app/modules/game/game.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PlanetModel } from 'src/app/@core/models/planet';
 
 @Component({
   selector: 'app-game',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameComponent implements OnInit {
 
+  public planet: PlanetModel = new PlanetModel();
+
   constructor() { }
 
   ngOnInit() {
+    this.planet = {
+      _id: 'sds586iosd',
+      name: 'Bespin',
+      population: '6000000',
+      climate: 'temperate',
+      terrain: 'gas giant',
+      films: 3
+    }
   }
 
 }
